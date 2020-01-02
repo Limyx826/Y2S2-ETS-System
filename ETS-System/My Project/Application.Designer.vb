03ee2b59dc336ed475913e637fd49637f034e961
Chong Yang Bei <cybc2000@outlook.com>
2020-01-03 05:31:21 +0800

    1

--- ETS-System/My Project/Application.Designer.vb
+++ ETS-System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ETS_System.FormMemReg
+            Me.MainForm = Global.ETS_System.Frontpage
         End Sub
     End Class
 End Namespace
