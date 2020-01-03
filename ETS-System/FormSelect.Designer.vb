c440734179424ee5ee48b4f7211d59ce8e503557
Chong Yang Bei <cybc2000@outlook.com>
2020-01-03 12:27:08 +0800

    FormSelect update

--- ETS-System/FormSelect.Designer.vb
+++ ETS-System/FormSelect.Designer.vb
@@ -31,26 +31,38 @@ Partial Class FormSelect
         Me.NextButton = New System.Windows.Forms.Button()
         Me.JCLODataSet = New ETS_System.JCLODataSet()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
         CType(Me.JCLODataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
         Me.SuspendLayout()
         '
         'ListView1
         '
         Me.ListView1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ListView1.HideSelection = False
-        Me.ListView1.Location = New System.Drawing.Point(58, 54)
+        Me.ListView1.Location = New System.Drawing.Point(44, 44)
+        Me.ListView1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(662, 87)
+        Me.ListView1.Size = New System.Drawing.Size(498, 71)
         Me.ListView1.TabIndex = 0
         Me.ListView1.UseCompatibleStateImageBehavior = False
         '
         'Back_button
         '
         Me.Back_button.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Back_button.Location = New System.Drawing.Point(58, 382)
+        Me.Back_button.Location = New System.Drawing.Point(44, 342)
+        Me.Back_button.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Back_button.Name = "Back_button"
-        Me.Back_button.Size = New System.Drawing.Size(75, 28)
+        Me.Back_button.Size = New System.Drawing.Size(56, 23)
         Me.Back_button.TabIndex = 1
         Me.Back_button.Text = "Back"
         Me.Back_button.UseVisualStyleBackColor = True
@@ -59,9 +71,10 @@ Partial Class FormSelect
         '
         Me.RB4.AutoSize = True
         Me.RB4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RB4.Location = New System.Drawing.Point(58, 312)
+        Me.RB4.Location = New System.Drawing.Point(5, 131)
+        Me.RB4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RB4.Name = "RB4"
-        Me.RB4.Size = New System.Drawing.Size(131, 24)
+        Me.RB4.Size = New System.Drawing.Size(112, 21)
         Me.RB4.TabIndex = 24
         Me.RB4.TabStop = True
         Me.RB4.Text = "RadioButton4"
@@ -71,9 +84,10 @@ Partial Class FormSelect
         '
         Me.RB3.AutoSize = True
         Me.RB3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RB3.Location = New System.Drawing.Point(58, 265)
+        Me.RB3.Location = New System.Drawing.Point(5, 92)
+        Me.RB3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RB3.Name = "RB3"
-        Me.RB3.Size = New System.Drawing.Size(131, 24)
+        Me.RB3.Size = New System.Drawing.Size(112, 21)
         Me.RB3.TabIndex = 23
         Me.RB3.TabStop = True
         Me.RB3.Text = "RadioButton3"
@@ -83,9 +97,10 @@ Partial Class FormSelect
         '
         Me.RB2.AutoSize = True
         Me.RB2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RB2.Location = New System.Drawing.Point(58, 220)
+        Me.RB2.Location = New System.Drawing.Point(5, 56)
+        Me.RB2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RB2.Name = "RB2"
-        Me.RB2.Size = New System.Drawing.Size(131, 24)
+        Me.RB2.Size = New System.Drawing.Size(112, 21)
         Me.RB2.TabIndex = 22
         Me.RB2.TabStop = True
         Me.RB2.Text = "RadioButton2"
@@ -95,9 +110,10 @@ Partial Class FormSelect
         '
         Me.RB1.AutoSize = True
         Me.RB1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RB1.Location = New System.Drawing.Point(58, 172)
+        Me.RB1.Location = New System.Drawing.Point(5, 17)
+        Me.RB1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RB1.Name = "RB1"
-        Me.RB1.Size = New System.Drawing.Size(131, 24)
+        Me.RB1.Size = New System.Drawing.Size(112, 21)
         Me.RB1.TabIndex = 21
         Me.RB1.TabStop = True
         Me.RB1.Text = "RadioButton1"
@@ -106,9 +122,10 @@ Partial Class FormSelect
         'NextButton
         '
         Me.NextButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.NextButton.Location = New System.Drawing.Point(681, 382)
+        Me.NextButton.Location = New System.Drawing.Point(511, 342)
+        Me.NextButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.NextButton.Name = "NextButton"
-        Me.NextButton.Size = New System.Drawing.Size(75, 28)
+        Me.NextButton.Size = New System.Drawing.Size(56, 23)
         Me.NextButton.TabIndex = 37
         Me.NextButton.Text = "Next"
         Me.NextButton.UseVisualStyleBackColor = True
@@ -122,32 +139,125 @@ Partial Class FormSelect
         '
         Me.PictureBox1.Image = Global.ETS_System.My.Resources.Resources.ETS
         Me.PictureBox1.Location = New System.Drawing.Point(0, 1)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(80, 31)
+        Me.PictureBox1.Size = New System.Drawing.Size(60, 25)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 38
         Me.PictureBox1.TabStop = False
         '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(44, 136)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(498, 201)
+        Me.TabControl1.TabIndex = 39
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.RB2)
+        Me.TabPage1.Controls.Add(Me.RB1)
+        Me.TabPage1.Controls.Add(Me.RB3)
+        Me.TabPage1.Controls.Add(Me.RB4)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(490, 175)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.RadioButton1)
+        Me.TabPage2.Controls.Add(Me.RadioButton2)
+        Me.TabPage2.Controls.Add(Me.RadioButton3)
+        Me.TabPage2.Controls.Add(Me.RadioButton4)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(490, 175)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(97, 134)
+        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(112, 21)
+        Me.RadioButton1.TabIndex = 28
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton4"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(97, 95)
+        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(112, 21)
+        Me.RadioButton2.TabIndex = 27
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton3"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(97, 59)
+        Me.RadioButton3.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(112, 21)
+        Me.RadioButton3.TabIndex = 26
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton2"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton4.Location = New System.Drawing.Point(97, 20)
+        Me.RadioButton4.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(112, 21)
+        Me.RadioButton4.TabIndex = 25
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "RadioButton1"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
         'FormSelect
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(600, 407)
+        Me.Controls.Add(Me.TabControl1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.NextButton)
-        Me.Controls.Add(Me.RB4)
-        Me.Controls.Add(Me.RB3)
-        Me.Controls.Add(Me.RB2)
-        Me.Controls.Add(Me.RB1)
         Me.Controls.Add(Me.Back_button)
         Me.Controls.Add(Me.ListView1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FormSelect"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "FormSelect"
         CType(Me.JCLODataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -160,4 +270,11 @@ Partial Class FormSelect
     Friend WithEvents NextButton As Button
     Friend WithEvents JCLODataSet As JCLODataSet
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
 End Class
